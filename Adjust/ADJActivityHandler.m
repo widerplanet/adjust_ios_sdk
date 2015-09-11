orta/ios_sdk

Assign delegate

Adjust/ADJActivityHandler.m
@@ -32,7 +32,7 @@ static const char * const kInternalQueueName     = "io.adjust.ActivityQueue";
 @property (nonatomic, retain) ADJActivityState *activityState;
 @property (nonatomic, retain) ADJTimerCycle *timer;
 @property (nonatomic, retain) id<ADJLogger> logger;
-@property (nonatomic, retain) NSObject<AdjustDelegate> *delegate;
+@property (nonatomic, assign) NSObject<AdjustDelegate> *delegate;
 @property (nonatomic, copy) ADJAttribution *attribution;
 @property (nonatomic, copy) ADJConfig *adjustConfig;
 

Adjust/ADJConfig.h
@@ -88,7 +88,7 @@
  * @param delegate The delegate that might implement the optional delegate
  *     methods like adjustAttributionChanged:
  */
-@property (nonatomic, retain) NSObject<AdjustDelegate> *delegate;
+@property (nonatomic, assign) NSObject<AdjustDelegate> *delegate;
 @property (nonatomic, assign) BOOL hasDelegate;
 
 - (BOOL) isValid;

widerplanet/adjust_ios_sdk

Adjust/ADJActivityHandler.m
@@ -32,7 +32,7 @@ static const char * const kInternalQueueName     = "io.adjust.ActivityQueue";
 @property (nonatomic, retain) ADJActivityState *activityState;
 @property (nonatomic, retain) ADJTimerCycle *timer;
 @property (nonatomic, retain) id<ADJLogger> logger;
-@property (nonatomic, retain) NSObject<AdjustDelegate> *delegate;
+@property (nonatomic, weak) NSObject<AdjustDelegate> *delegate;
 @property (nonatomic, copy) ADJAttribution *attribution;
 @property (nonatomic, copy) ADJConfig *adjustConfig;
 

Adjust/ADJConfig.h
@@ -88,7 +88,7 @@
  * @param delegate The delegate that might implement the optional delegate
  *     methods like adjustAttributionChanged:
  */
-@property (nonatomic, retain) NSObject<AdjustDelegate> *delegate;
+@property (nonatomic, weak) NSObject<AdjustDelegate> *delegate;
 @property (nonatomic, assign) BOOL hasDelegate;
 
 - (BOOL) isValid;