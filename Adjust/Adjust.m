41d8e91c92f3bbe7f5b54d059d4d508d63d0e455
uerceg <ugljesa@adjust.com>
2016-08-23 15:52:26 +0200

    New Xamarin version 4.8.0

--- Adjust/Adjust.m
+++ Adjust/Adjust.m
@@ -140,7 +140,7 @@ NSString * const ADJEnvironmentProduction   = @"production";
     self.activityHandler = [ADJAdjustFactory activityHandlerWithConfig:adjustConfig
                                         sessionParametersActionsArray:self.sessionParametersActionsArray];
     
-    [adjustConfig setSdkPrefix:@"xamarin4.0.2"];
+    [adjustConfig setSdkPrefix:@"xamarin4.8.0"];
     self.activityHandler = [ADJAdjustFactory activityHandlerWithConfig:adjustConfig];
 }
 
