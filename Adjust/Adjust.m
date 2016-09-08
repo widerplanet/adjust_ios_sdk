100fd850bf20c3e7d78afdf7f100145934a3fbef
uerceg <ugljesa@adjust.com>
2016-09-08 10:00:52 +0200

    Xamarin iOS prefix 4.9.0

--- Adjust/Adjust.m
+++ Adjust/Adjust.m
@@ -140,7 +140,7 @@ NSString * const ADJEnvironmentProduction   = @"production";
     self.activityHandler = [ADJAdjustFactory activityHandlerWithConfig:adjustConfig
                                         sessionParametersActionsArray:self.sessionParametersActionsArray];
     
-    [adjustConfig setSdkPrefix:@"xamarin4.8.0"];
+    [adjustConfig setSdkPrefix:@"xamarin4.9.0"];
     self.activityHandler = [ADJAdjustFactory activityHandlerWithConfig:adjustConfig];
 }
 
