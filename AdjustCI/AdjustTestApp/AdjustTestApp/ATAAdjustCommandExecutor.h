f940be30482fd2c954f9f2e3f5c16292ca629910
nonelse <pedro@adjust.com>
2017-08-24 16:41:33 +0200

    Update test app

---
+++ AdjustCI/AdjustTestApp/AdjustTestApp/ATAAdjustCommandExecutor.h
@@ -0,0 +1,14 @@
+//
+//  ATAAdjustCommandExecutor.h
+//  AdjustTestApp
+//
+//  Created by Pedro on 23.08.17.
+//  Copyright © 2017 adjust. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ATLTestLibrary.h"
+
+@interface ATAAdjustCommandExecutor : NSObject<AdjustCommandDelegate>
+
+@end
