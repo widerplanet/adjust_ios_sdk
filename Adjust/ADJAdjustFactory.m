alloy/ios_sdk

Check attribution from click response

Adjust/ADJAdjustFactory.m
@@ -122,13 +122,14 @@ static NSTimeInterval internalMaxDelayStart = -1;
                                                  startsSending:startsSending];
 }
 
-+ (id<ADJSdkClickHandler>)sdkClickHandlerWithStartsPaused:(BOOL)startsSending
++ (id<ADJSdkClickHandler>)sdkClickHandlerWithStartsPaused:(id<ADJActivityHandler>)activityHandler
+                                            startsSending:(BOOL)startsSending
 {
     if (internalSdkClickHandler == nil) {
-        return [ADJSdkClickHandler handlerWithStartsSending:startsSending];
+        return [ADJSdkClickHandler handlerWithStartsSending:activityHandler startsSending:startsSending];
     }
 
-    return [internalSdkClickHandler initWithStartsSending:startsSending];
+    return [internalSdkClickHandler initWithStartsSending:activityHandler startsSending:startsSending];
 }
 
 + (BOOL)testing {

widerplanet/adjust_ios_sdk

Adjust/ADJAdjustFactory.m
@@ -122,13 +122,14 @@ static NSTimeInterval internalMaxDelayStart = -1;
                                                  startsSending:startsSending];
 }
 
-+ (id<ADJSdkClickHandler>)sdkClickHandlerWithStartsPaused:(BOOL)startsSending
++ (id<ADJSdkClickHandler>)sdkClickHandlerWithStartsPaused:(id<ADJActivityHandler>)activityHandler
+                                            startsSending:(BOOL)startsSending
 {
     if (internalSdkClickHandler == nil) {
-        return [ADJSdkClickHandler handlerWithStartsSending:startsSending];
+        return [ADJSdkClickHandler handlerWithActivityHandler:activityHandler startsSending:startsSending];
     }
 
-    return [internalSdkClickHandler initWithStartsSending:startsSending];
+    return [internalSdkClickHandler initWithActivityHandler:activityHandler startsSending:startsSending];
 }
 
 + (BOOL)testing {