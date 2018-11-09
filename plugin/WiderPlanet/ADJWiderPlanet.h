5559b986aa3317f0bc9844b754dc49f1d63d760a
Michael Kim <michael@widerplanet.com>
2018-11-09 09:59:20 +0900

    Wider Planet's TargetingGates Module

---
+++ plugin/WiderPlanet/ADJWiderPlanet.h
@@ -0,0 +1,62 @@
+//
+//  ADJWiderPlanet.h
+//
+//
+//  Created by WiderPlanet on 12/21/17.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+#import "ADJEvent.h"
+
+@interface ADJWiderPlanetProduct : NSObject
+
+@property (nonatomic, assign) float widerplanetPrice;
+
+@property (nonatomic, assign) NSUInteger widerplanetQuantity;
+
+@property (nonatomic, copy, nullable) NSString *widerplanetProductID;
+
+- (nullable id)initWithId:(nullable NSString *)product_id price:(float)price quantity:(NSUInteger)quantity;
+
++ (nullable ADJWiderPlanetProduct *)productWithId:(nullable NSString *)product_id price:(float)price quantity:(NSUInteger)quantity;
+
+@end
+
+@interface ADJWiderPlanet : NSObject
+
++ (void)injectPartnerIdIntoWiderPlanetEvents:(nullable NSString *)client_id;
+
++ (void)injectCustomerIdIntoWiderPlanetEvents:(nullable NSString *)customerId;
+
++ (void)injectHashedEmailIntoWiderPlanetEvents:(nullable NSString *)hashUserId;
+
++ (void)injectHashedEmailIntoWiderPlanetEvents:(nullable NSString *)hashEmail;
+
++ (void)injectDeeplinkIntoEvent:(nullable ADJEvent *)event url:(nullable NSURL *)url;
+
++ (void)injectCartIntoEvent:(nullable ADJEvent *)event products:(nullable NSArray *)products;
+
++ (void)injectUserLevelIntoEvent:(nullable ADJEvent *)event uiLevel:(NSUInteger)uiLevel;
+
++ (void)injectCustomEventIntoEvent:(nullable ADJEvent *)event uiData:(nullable NSString *)uiData;
+
++ (void)injectUserStatusIntoEvent:(nullable ADJEvent *)event uiStatus:(nullable NSString *)uiStatus;
+
++ (void)injectViewProductIntoEvent:(nullable ADJEvent *)event product_id:(nullable NSString *)product_id;
+
++ (void)injectViewListingIntoEvent:(nullable ADJEvent *)event product_ids:(nullable NSArray *)product_ids;
+
++ (void)injectAchievementUnlockedIntoEvent:(nullable ADJEvent *)event uiAchievement:(nullable NSString *)uiAchievement;
+
++ (void)injectViewSearchDatesIntoWiderPlanetEvents:(nullable NSString *)checkInDate checkOutDate:(nullable NSString *)checkOutDate;
+
++ (void)injectCustomEvent2IntoEvent:(nullable ADJEvent *)event uiData2:(nullable NSString *)uiData2 uiData3:(NSUInteger)uiData3;
+
++ (void)injectTransactionConfirmedIntoEvent:(nullable ADJEvent *)event
+                                   products:(nullable NSArray *)products
+                              transactionId:(nullable NSString *)transactionId
+                                newCustomer:(nullable NSString *)newCustomer;
+
+@end
---
+++ plugin/WiderPlanet/ADJWiderPlanet.m
@@ -0,0 +1,283 @@
+//
+//  ADJWiderPlanet.m
+//
+//
+//  Created by WiderPlanet on 12/21/17.
+//
+//
+
+#import "Adjust.h"
+#import "ADJWiderPlanet.h"
+#import "ADJAdjustFactory.h"
+
+static const NSUInteger MAX_VIEW_LISTING_PRODUCTS = 10;
+
+@implementation ADJWiderPlanetProduct
+
+- (id)initWithId:(NSString *)product_id price:(float)price quantity:(NSUInteger)quantity {
+    self = [super init];
+
+    if (self == nil) {
+        return nil;
+    }
+
+    self.widerplanetPrice = price;
+    self.widerplanetQuantity = quantity;
+    self.widerplanetProductID = product_id;
+
+    return self;
+}
+
++ (ADJWiderPlanetProduct *)productWithId:(NSString *)product_id price:(float)price quantity:(NSUInteger)quantity {
+    return [[ADJWiderPlanetProduct alloc] initWithId:product_id price:price quantity:quantity];
+}
+
+@end
+
+@implementation ADJWiderPlanet
+
+static NSString * hashEmailInternal;
+static NSString * client_idInternal;
+static NSString * hashUIDInternal;
+static NSString * customerIdInternal;
+static NSString * userSegmentInternal;
+static NSString * checkInDateInternal;
+static NSString * checkOutDateInternal;
+
++ (id<ADJLogger>)logger {
+    return ADJAdjustFactory.logger;
+}
+
++ (void)injectViewSearchIntoEvent:(ADJEvent *)event checkInDate:(NSString *)din checkOutDate:(NSString *)dout {
+    [event addPartnerParameter:@"din" value:din];
+    [event addPartnerParameter:@"dout" value:dout];
+
+    [ADJWiderPlanet injectOptionalParams:event];
+}
+
++ (void)injectViewListingIntoEvent:(ADJEvent *)event product_ids:(NSArray *)product_ids {
+    NSString *jsonProductsIds = [ADJWiderPlanet createWiderPlanetVLFromProducts:product_ids];
+    [event addPartnerParameter:@"items" value:jsonProductsIds];
+
+    [ADJWiderPlanet injectOptionalParams:event];
+}
+
++ (void)injectViewProductIntoEvent:(ADJEvent *)event product_id:(NSString *)product_id {
+    [event addPartnerParameter:@"items" value:product_id];
+
+    [ADJWiderPlanet injectOptionalParams:event];
+}
+
++ (void)injectCartIntoEvent:(ADJEvent *)event products:(NSArray *)products {
+    NSString *jsonProducts = [ADJWiderPlanet createWiderPlanetVBFromProducts:products];
+    [event addPartnerParameter:@"items" value:jsonProducts];
+
+    [ADJWiderPlanet injectOptionalParams:event];
+}
+
++ (void)injectTransactionConfirmedIntoEvent:(ADJEvent *)event
+                                   products:(NSArray *)products
+                              transactionId:(NSString *)transactionId
+                                newCustomer:(NSString *)newCustomer {
+    [event addPartnerParameter:@"order_id" value:transactionId];
+
+    NSString *jsonProducts = [ADJWiderPlanet createWiderPlanetVBFromProducts:products];
+    [event addPartnerParameter:@"items" value:jsonProducts];
+    [event addPartnerParameter:@"new_customer" value:newCustomer];
+
+    [ADJWiderPlanet injectOptionalParams:event];
+}
+
++ (void)injectUserLevelIntoEvent:(ADJEvent *)event uiLevel:(NSUInteger)uiLevel {
+    NSString *uiLevelString = [NSString stringWithFormat:@"%lu",(unsigned long)uiLevel];
+    [event addPartnerParameter:@"ui_level" value:uiLevelString];
+
+    [ADJWiderPlanet injectOptionalParams:event];
+}
+
++ (void)injectUserStatusIntoEvent:(ADJEvent *)event uiStatus:(NSString *)uiStatus {
+    [event addPartnerParameter:@"ui_status" value:uiStatus];
+
+    [ADJWiderPlanet injectOptionalParams:event];
+}
+
++ (void)injectAchievementUnlockedIntoEvent:(ADJEvent *)event uiAchievement:(NSString *)uiAchievement {
+    [event addPartnerParameter:@"ui_achievmnt" value:uiAchievement];
+
+    [ADJWiderPlanet injectOptionalParams:event];
+}
+
++ (void)injectCustomEventIntoEvent:(ADJEvent *)event uiData:(NSString *)uiData {
+    [event addPartnerParameter:@"ui_data" value:uiData];
+
+    [ADJWiderPlanet injectOptionalParams:event];
+}
+
++ (void)injectCustomEvent2IntoEvent:(ADJEvent *)event uiData2:(NSString *)uiData2 uiData3:(NSUInteger)uiData3 {
+    [event addPartnerParameter:@"ui_data2" value:uiData2];
+
+    NSString *uiData3String = [NSString stringWithFormat:@"%lu",(unsigned long)uiData3];
+    [event addPartnerParameter:@"ui_data3" value:uiData3String];
+
+    [ADJWiderPlanet injectOptionalParams:event];
+}
+
++ (void)injectDeeplinkIntoEvent:(ADJEvent *)event url:(NSURL *)url {
+    if (url == nil) {
+        return;
+    }
+
+    [event addPartnerParameter:@"loc" value:[url absoluteString]];
+
+    [ADJWiderPlanet injectOptionalParams:event];
+}
+
++ (void)injectHashedEmailIntoWiderPlanetEvents:(NSString *)hashEmail {
+    hashEmailInternal = hashEmail;
+}
+
++ (void)injectViewSearchDatesIntoWiderPlanetEvents:(NSString *)checkInDate checkOutDate:(NSString *)checkOutDate {
+    checkInDateInternal = checkInDate;
+    checkOutDateInternal = checkOutDate;
+}
+
++ (void)injectClientIdIntoWiderPlanetEvents:(NSString *)client_id {
+    client_idInternal = client_id;
+}
+
++ (void)injectUserSegmentIntoWiderPlanetEvents:(NSString *)userSegment {
+    userSegmentInternal = userSegment;
+}
+
++ (void)injectCustomerIdIntoWiderPlanetEvents:(NSString *)customerId {
+    customerIdInternal = customerId;
+}
+
++ (void)injectOptionalParams:(ADJEvent *)event {
+    [ADJWiderPlanet injectHashEmail:event];
+    [ADJWiderPlanet injectSearchDates:event];
+    [ADJWiderPlanet injectClientId:event];
+    [ADJWiderPlanet injectCustomerId:event];
+}
+
++ (void)injectHashEmail:(ADJEvent *)event {
+    if (hashEmailInternal == nil) {
+        return;
+    }
+
+    [event addPartnerParameter:@"hcuid" value:hashEmailInternal];
+}
+
++ (void)injectSearchDates:(ADJEvent *)event {
+    if (checkInDateInternal == nil || checkOutDateInternal == nil) {
+        return;
+    }
+
+    [event addPartnerParameter:@"din" value:checkInDateInternal];
+    [event addPartnerParameter:@"dout" value:checkOutDateInternal];
+}
+
++ (void)injectClientId:(ADJEvent *)event {
+    if (client_idInternal == nil) {
+        return;
+    }
+
+    [event addPartnerParameter:@"client_id" value:client_idInternal];
+}
+
+
++ (void)injectCustomerId:(ADJEvent *)event {
+    if (customerIdInternal == nil) {
+        return;
+    }
+
+    [event addPartnerParameter:@"customer_id" value:customerIdInternal];
+}
+
++ (NSString *)createWiderPlanetVBFromProducts:(NSArray *)products {
+    if (products == nil) {
+        [self.logger warn:@"WiderPlanet Event product list is nil. It will sent as empty."];
+        products = @[];
+    }
+
+    NSUInteger productsCount = [products count];
+    NSMutableString *widerplanetVBValue = [NSMutableString stringWithString:@"["];
+    
+    for (NSUInteger i = 0; i < productsCount;) {
+        id productAtIndex = [products objectAtIndex:i];
+
+        if (![productAtIndex isKindOfClass:[ADJWiderPlanetProduct class]]) {
+            [self.logger error:@"WiderPlanet Event should contain a list of ADJWiderPlanetProduct"];
+            return nil;
+        }
+
+        ADJWiderPlanetProduct *product = (ADJWiderPlanetProduct *)productAtIndex;
+        NSString *productString = [NSString stringWithFormat:@"{\"event_item_id\":\"%@\",\"unit_price\":%f,\"quantity\":%lu}",
+                                   [product widerplanetProductID],
+                                   [product widerplanetPrice],
+                                   (unsigned long)[product widerplanetQuantity]];
+
+        [widerplanetVBValue appendString:productString];
+
+        i++;
+
+        if (i == productsCount) {
+            break;
+        }
+
+        [widerplanetVBValue appendString:@","];
+    }
+
+    [widerplanetVBValue appendString:@"]"];
+
+    return [widerplanetVBValue stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
+}
+
++ (NSString *)createWiderPlanetVLFromProducts:(NSArray *)product_ids {
+    if (product_ids == nil) {
+        [self.logger warn:@"WiderPlanet View Listing product ids list is nil. It will sent as empty."];
+        product_ids = @[];
+    }
+
+    NSUInteger productsIdCount = [product_ids count];
+
+    if (productsIdCount > MAX_VIEW_LISTING_PRODUCTS) {
+        [self.logger warn:@"WiderPlanet View Listing should only have at most 3 product ids. The rest will be discarded."];
+    }
+
+    NSMutableString *widerplanetVLValue = [NSMutableString stringWithString:@"["];
+
+    for (NSUInteger i = 0; i < productsIdCount;) {
+        id productAtIndex = [product_ids objectAtIndex:i];
+        NSString *product_id;
+        
+        if ([productAtIndex isKindOfClass:[NSString class]]) {
+            product_id = productAtIndex;
+        } else if ([productAtIndex isKindOfClass:[ADJWiderPlanetProduct class]]) {
+            ADJWiderPlanetProduct *widerplanetProduct = (ADJWiderPlanetProduct *)productAtIndex;
+            product_id = [widerplanetProduct widerplanetProductID];
+            
+            [self.logger warn:@"WiderPlanet View Listing should contain a list of product ids, not of ADJWiderPlanetProduct. Reading the product id of the ADJWiderPlanetProduct."];
+        } else {
+            return nil;
+        }
+
+        NSString *product_idEscaped = [NSString stringWithFormat:@"\"%@\"", product_id];
+
+        [widerplanetVLValue appendString:product_idEscaped];
+
+        i++;
+
+        if (i == productsIdCount || i >= MAX_VIEW_LISTING_PRODUCTS) {
+            break;
+        }
+
+        [widerplanetVLValue appendString:@","];
+    }
+
+    [widerplanetVLValue appendString:@"]"];
+
+    return [widerplanetVLValue stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
+}
+
+@end
